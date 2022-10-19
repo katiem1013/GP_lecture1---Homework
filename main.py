2248de4525d29d9093284c407d614c73b265a0f1
Katiem1013 <katiem1013@hotmail.co.uk>
2022-10-19 18:02:47 +0100

    Word Homework

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ word = str(input("Please enter a word: "))
 if (len(word) >= 3) & ("ing" not in word):
     print(word + "ing")
 
-if (len(word) >= 3) & ("ing" in word):
+elif (len(word) >= 3) & ("ing" in word):
     print(word + "ly")
 
 else:
     print(word)
+
+print("")
+print("Hello...")
